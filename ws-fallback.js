6a2776fef20474fdcd75abb063ba76b830ea82b3
Matteo Collina <hello@matteocollina.com>
2016-06-06 08:56:39 +0200

    Use typeof for checking globals.

--- ws-fallback.js
+++ ws-fallback.js
@@ -1,1 +1,12 @@
-module.exports = WebSocket || MozWebSocket || window.WebSocket || window.MozWebSocket
+
+var ws = null
+
+if (typeof WebSocket !== 'undefined') {
+  ws = WebSocket
+} else if (typeof MozWebSocket !== 'undefined') {
+  ws = MozWebSocket
+} else {
+  ws = window.WebSocket || window.MozWebSocket
+}
+
+module.exports = ws
