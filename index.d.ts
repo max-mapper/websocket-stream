b3cd795dc5979cd19cfa3c22fbcaa34aaad41919
Bjornskjald <github@bjorn.ml>
2020-02-15 20:51:46 +0100

    Fix incorrect typings for Server class

--- index.d.ts
+++ index.d.ts
@@ -10,12 +10,12 @@ declare namespace WebSocketStream {
   type WebSocketDuplex = Duplex & { socket: WebSocket };
 
   class Server extends WebSocket.Server {
-    on(event: 'connection', cb: (this: WebSocket, socket: WebSocket, request: http.IncomingMessage) => void): this;
-    on(event: 'error', cb: (this: WebSocket, error: Error) => void): this;
-    on(event: 'headers', cb: (this: WebSocket, headers: string[], request: http.IncomingMessage) => void): this;
-    on(event: 'listening', cb: (this: WebSocket) => void): this;
-    on(event: 'stream', cb: (this: WebSocket, stream: WebSocketDuplex, request: http.IncomingMessage) => void): this;
-    on(event: string | symbol, listener: (this: WebSocket, ...args: any[]) => void): this;
+    on(event: 'connection', cb: (this: WebSocket.Server, socket: WebSocket, request: http.IncomingMessage) => void): this;
+    on(event: 'error', cb: (this: WebSocket.Server, error: Error) => void): this;
+    on(event: 'headers', cb: (this: WebSocket.Server, headers: string[], request: http.IncomingMessage) => void): this;
+    on(event: 'listening', cb: (this: WebSocket.Server) => void): this;
+    on(event: 'stream', cb: (this: WebSocket.Server, stream: WebSocketDuplex, request: http.IncomingMessage) => void): this;
+    on(event: string | symbol, listener: (this: WebSocket.Server, ...args: any[]) => void): this;
   }
 
   function createServer(opts?: WebSocket.ServerOptions, callback?: () => void): Server;
