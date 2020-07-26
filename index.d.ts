c04c4e53884dcc44f71a035fafbcbf6265b2501c
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2020-07-26 12:11:11 +0300

    Update dependencies

--- index.d.ts
+++ index.d.ts
@@ -10,10 +10,10 @@ declare namespace WebSocketStream {
   type WebSocketDuplex = Duplex & { socket: WebSocket };
 
   class Server extends WebSocket.Server {
-    on(event: 'connection', cb: (this: WebSocket, socket: WebSocket, request: http.IncomingMessage) => void): this;
-    on(event: 'error', cb: (this: WebSocket, error: Error) => void): this;
-    on(event: 'headers', cb: (this: WebSocket, headers: string[], request: http.IncomingMessage) => void): this;
-    on(event: 'listening', cb: (this: WebSocket) => void): this;
+    on(event: 'connection', cb: (this: WebSocket.Server, socket: WebSocket, request: http.IncomingMessage) => void): this;
+    on(event: 'error', cb: (this: WebSocket.Server, error: Error) => void): this;
+    on(event: 'headers', cb: (this: WebSocket.Server, headers: string[], request: http.IncomingMessage) => void): this;
+    on(event: 'listening', cb: (this: WebSocket.Server) => void): this;
     on(event: 'stream', cb: (this: WebSocket, stream: WebSocketDuplex, request: http.IncomingMessage) => void): this;
     on(event: string | symbol, listener: (this: WebSocket, ...args: any[]) => void): this;
   }
